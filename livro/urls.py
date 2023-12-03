8d76cd20df8077a197cec10395c3b1bdd147dd78
Clebxx <thehunterpjm@gmail.com>
2023-12-03 11:51:46 -0300

    (feat): ListView para todas as entidades

--- livro/urls.py
+++ livro/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home.as_view(), name='Home page')
+  path('', views.home.as_view(), name='Home page'),
+  path('livros', views.livro_list.as_view(), name='livros'),
+  path('autores', views.autor_list.as_view(), name='autores'),
+  path('editoras', views.editora_list.as_view(), name='editoras'),
+  path('generos', views.genero_list.as_view(), name='generos'),
 ]
--- livro/views.py
+++ livro/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic.list import ListView
+from .models import Livro, Autor, Editora, Genero
 
 class home(View):
     def get(self, request):
         return render(request, 'home-page.html')
+
+class livro_list(ListView):
+    model = Livro
+    
+    def get(self, request):
+        return render(request, 'objetos.html', {'objects': Livro.objects.all(), 'Model': 'livros'})
+
+class autor_list(ListView):
+    model = Autor
+
+    def get(self, request):
+        return render(request, 'objetos.html', {'objects': Autor.objects.all(), 'Model': 'autores'})
+
+class editora_list(ListView):
+    model = Editora
+
+    def get(self, request):
+        return render(request, 'objetos.html', {'objects': Editora.objects.all(), 'Model': 'editoras'})
+    
+class genero_list(ListView):
+    model = Genero
+
+    def get(self, request):
+        return render(request, 'objetos.html', {'objects': Genero.objects.all(), 'Model': 'generos'})
